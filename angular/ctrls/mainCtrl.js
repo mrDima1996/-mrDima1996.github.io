a2cb9fd36396cf49ca148cfdf053b94418663900
mr.Dima1996 <mr.dima1996@gmail.com>
2016-01-29 13:04:02 +0200

    test

--- angular/ctrls/mainCtrl.js
+++ angular/ctrls/mainCtrl.js
@@ -2,8 +2,8 @@
  * Created by mrDima on 11.01.2016.
  */
 angular.module("app")
-    .controller('mainCtrl', [ '$scope','popUp', function($scope, popUp) {
-        popUp.changePopUpLogo();
+    .controller('mainCtrl', [ '$scope', function($scope) {
+        //popUp.changePopUpLogo();
         $scope.currentDate = new Date(2016, 0,30);
         $scope.year = $scope.currentDate.getFullYear();
     }]);
