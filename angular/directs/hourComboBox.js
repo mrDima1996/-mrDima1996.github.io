abd85fc3a17de0a4b6eada72c80821ec64f9311c
mr.Dima1996 <mr.dima1996@gmail.com>
2016-02-03 15:34:55 +0200

    добавил неподгрузившиеся файлы выпадающего списка

---
+++ angular/directs/hourComboBox.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mrDima on 03.02.2016.
+ */
+angular.module('app')
+
+.directive('hourComboBox', function(){
+        return {
+            templateUrl: 'angular/htmls/hourComboBox.html'
+        }
+    });
